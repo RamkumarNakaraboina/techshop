3d4b8c42df85888a3c99b701a3a6358e6b751855
NAKARABOYINA RAM KUMAR <nakhraboinaramkumar@gmail.com>
2025-11-25 21:08:49 +0530

    updated Datas

--- src/projectData/Data.js
+++ src/projectData/Data.js
@@ -1,15 +1,87 @@
+// import firsts from "../images/products/boat110-1.png";
+import firsts from "../images/products/fourthSlider.png";
+import second from "../images/products/jbl660nc-2.png";
+import third from "../images/products/jbl660nc-3.png";
+import fourth from "../images/products/jbl660nc-4.png";
+import fifth from "../images/products/boat518-1.png";
+import seven from "../images/products/boat518-2.png";
+import eight from "../images/products/boat518-3.png";
+import nine from "../images/products/boat518-4.png";
+import ten from "../images/products/boat131-1.png";
+import elven from "../images/products/boat131-2.png";
+import twelve from "../images/products/boat131-3.png";
+import thirteen from "../images/products/secondSlider.png";
+import fourteen from "../images/products/boat110-1.png";
+import fifteen from "../images/products/boat110-2.png";
+import sisteen from "../images/products/boat110-3.png";
+import seventeen from "../images/products/boat110-4.png";
+import eigthteen from "../images/products/boat410-1.png";
+import nineteen from "../images/products/boat410-2.png";
+import twenty from "../images/products/boat410-3.png";
+import twentyone from "../images/products/boat410-4.png";
+import twentyTwo from "../images/products/jbl200bt-1.png";
+import twentyThree from "../images/products/jbl200bt-2.png";
+import twentyFour from "../images/products/jbl200bt-3.png";
+import twentyFive from "../images/products/jbl200bt-4.png";
+import twentySix from "../images/products/fourthSlider.png";
+import twentySeven from "../images/products/sonyXb910n-2.png";
+import twentyEight from "../images/products/sonyXb910n-3.png";
+import twentyNine from "../images/products/sonyXb910n-4.png";
+import thirteens from "../images/products/jbl760nc-1.png";
+import thirteenone from "../images/products/jbl760nc-2.png";
+import thirteenTwo from "../images/products/jbl760nc-3.png";
+import thirteenThree from "../images/products/jbl760nc-4.png";
+import thirteenfour from "../images/products/boat255r-1.png";
+import thirteenfive from "../images/products/boat255r-2.png";
+import thirteenSix from "../images/products/boat255r-3.png";
+import thirteenSeven from "../images/products/boat255r-4.png";
+import thirteenEight from "../images/products/jbl100-1.png";
+import thirteenNine from "../images/products/jbl100-2.png";
+import fourty from "../images/products/jbl100-3.png";
+import fourtyOne from "../images/products/jbl100-4.png";
+import fourtyTwo from "../images/products/sony1000xm4-1.png";
+import fourtyThree from "../images/products/sony1000xm4-2.png";
+import fourtyFour from "../images/products/sony1000xm4-3.png";
+import fourtyFive from "../images/products/sony1000xm4-4.png";
+import fourtySix from "../images/products/boat228-1.png";
+import fourtySeven from "../images/products/boat228-2.png";
+import fourtyEight from "../images/products/fourthSlider.png";
+import fourtyNine from "../images/products/boat228-4.png";
+import fifty from "../images/products/jbl-endu-1.png";
+import fiftyone from "../images/products/jbl-endu-2.png";
+import fiftytwo from "../images/products/jbl-endu-3.png";
+import fiftyThree from "../images/products/jbl-endu-4.png";
+import fiftyFour from "../images/products/boat203-1.png";
+import fiftyFive from "../images/products/boat203-2.png";
+import fiftySix from "../images/products/boat203-3.png";
+import fiftySeven from "../images/products/boat203-4.png";
+import fiftyEight from "../images/products/sonyCh710n-1.png";
+import fiftyNine from "../images/products/sonyCh710n-2.png";
+import sixty from "../images/products/sonyCh710n-3.png";
+import sixtyOne from "../images/products/sonyCh710n-4.png";
+import sixtyTwo from "../images/products/jbl500bt-1.png";
+import sixtyThree from "../images/products/jbl500bt-2.png";
+import sixtyFour from "../images/products/jbl500bt-3.png";
+import sixtyFive from "../images/products/jbl500bt-4.png";
+import sixtySix from "../images/products/boat381-1.png";
+import sixtySeven from "../images/products/boat381-2.png";
+import sixtyEight from "../images/products/boat381-2.png";
+import sixtyNine from "../images/products/boat381-4.png";
+import seventy from "../images/products/sony-ex14ap-1.png";
+import seventyOne from "../images/products/sony-ex14ap-2.png";
+import seventyTwo from "../images/products/sony-ex14ap-3.png";
+import seventyThree from "../images/products/sony-ex14ap-4.png";
+import seventyFour from "../images/products/sonyXb400-1.png";
+import sevetyFive from "../images/products/sonyXb400-2.png";
+import seventySix from "../images/products/sonyXb400-3.png";
+import seventySeven from "../images/products/sonyXb400-4.png";
 const productsData = [
   {
     id: 1,
     tag: "hero-product",
     tagline: "Keep the noise out, or in. You choose.",
     heroImage: "/images/products/jbl660nc-1.png",
-    images: [
-      "/images/products/jbl660nc-1.png",
-      "/images/products/jbl660nc-2.png",
-      "/images/products/jbl660nc-3.png",
-      "/images/products/jbl660nc-4.png",
-    ],
+    images: [firsts, second, third, fourth],
     brand: "JBL",
     title: "JBL Live 660NC",
     info: "Wireless Over-Ear NC Headphones",
@@ -26,12 +98,7 @@ const productsData = [
   {
     id: 2,
     tag: "featured-product",
-    images: [
-      "/images/products/boat518-1.png",
-      "/images/products/boat518-2.png",
-      "/images/products/boat518-3.png",
-      "/images/products/boat518-4.png",
-    ],
+    images: [fifth, seven, eight, nine],
     brand: "boAt",
     title: "boAt Rockerz 518",
     info: "On-Ear Wireless Headphones",
@@ -50,12 +117,7 @@ const productsData = [
     tag: "hero-product",
     tagline: "Featherweight for comfort all-day.",
     heroImage: "/images/products/boat131-3.png",
-    images: [
-      "/images/products/boat131-1.png",
-      "/images/products/boat131-2.png",
-      "/images/products/boat131-3.png",
-      "/images/products/boat131-4.png",
-    ],
+    images: [ten, elven, twelve, thirteen],
     brand: "boAt",
     title: "boAt Airdopes 131",
     info: "Wireless In-Ear Earbuds",
@@ -71,12 +133,7 @@ const productsData = [
   },
   {
     id: 4,
-    images: [
-      "/images/products/boat110-1.png",
-      "/images/products/boat110-2.png",
-      "/images/products/boat110-3.png",
-      "/images/products/boat110-4.png",
-    ],
+    images: [fourteen, fifteen, sisteen, seventeen],
     brand: "boAt",
     title: "boAt BassHeads 110",
     info: "In-Ear Wired Earphones",
@@ -92,12 +149,7 @@ const productsData = [
   },
   {
     id: 5,
-    images: [
-      "/images/products/boat410-1.png",
-      "/images/products/boat410-2.png",
-      "/images/products/boat410-3.png",
-      "/images/products/boat410-4.png",
-    ],
+    images: [eigthteen, nineteen, twenty, twentyone],
     brand: "boAt",
     title: "boAt Rockerz 410",
     info: "Bluetooth & Wired On-Ear Headphones",
@@ -113,12 +165,7 @@ const productsData = [
   },
   {
     id: 6,
-    images: [
-      "/images/products/jbl200bt-1.png",
-      "/images/products/jbl200bt-2.png",
-      "/images/products/jbl200bt-3.png",
-      "/images/products/jbl200bt-4.png",
-    ],
+    images: [twentyTwo, twentyThree, twentyFour, twentyFive],
     brand: "JBL",
     title: "JBL Live 200BT",
     info: "In-Ear Wireless Neckbands",
@@ -136,13 +183,8 @@ const productsData = [
     id: 7,
     tag: "hero-product",
     tagline: "Give your favourite music a boost.",
-    heroImage: "/images/products/sonyXb910n-1.png",
-    images: [
-      "/images/products/sonyxb910n-1.png",
-      "/images/products/sonyxb910n-2.png",
-      "/images/products/sonyxb910n-3.png",
-      "/images/products/sonyxb910n-4.png",
-    ],
+    heroImage: "/images/products/sonyxb910n-1.png",
+    images: [twentySix, twentySeven, twentyEight, twentyNine],
     brand: "Sony",
     title: "Sony WH-XB910N",
     info: "Wireless Over-Ear Headphones",
@@ -159,12 +201,7 @@ const productsData = [
   {
     id: 8,
     tag: "featured-product",
-    images: [
-      "/images/products/jbl760nc-1.png",
-      "/images/products/jbl760nc-2.png",
-      "/images/products/jbl760nc-3.png",
-      "/images/products/jbl760nc-4.png",
-    ],
+    images: [thirteens, thirteenone, thirteenTwo, thirteenThree],
     brand: "JBL",
     title: "JBL Tune 760NC",
     info: "Wireless Over-Ear NC Headphones",
@@ -181,12 +218,7 @@ const productsData = [
   {
     id: 9,
     tag: "featured-product",
-    images: [
-      "/images/products/boat255r-1.png",
-      "/images/products/boat255r-2.png",
-      "/images/products/boat255r-3.png",
-      "/images/products/boat255r-4.png",
-    ],
+    images: [thirteenfour, thirteenfive, thirteenSix, thirteenSeven],
     brand: "boAt",
     title: "boAt Rockerz 255",
     info: "In-Ear Wireless Neckbands",
@@ -202,12 +234,7 @@ const productsData = [
   },
   {
     id: 10,
-    images: [
-      "/images/products/jbl100-1.png",
-      "/images/products/jbl100-2.png",
-      "/images/products/jbl100-3.png",
-      "/images/products/jbl100-4.png",
-    ],
+    images: [thirteenEight, thirteenNine, fourty, fourtyOne],
     brand: "JBL",
     title: "JBL Wave 100",
     info: "In-Ear Truly Wireless Earbuds",
@@ -223,12 +250,7 @@ const productsData = [
   },
   {
     id: 11,
-    images: [
-      "/images/products/sony1000xm4-1.png",
-      "/images/products/sony1000xm4-2.png",
-      "/images/products/sony1000xm4-3.png",
-      "/images/products/sony1000xm4-4.png",
-    ],
+    images: [fourtyTwo, fourtyThree, fourtyFour, fourtyFive],
     brand: "Sony",
     title: "Sony WF-1000XM4",
     info: "Wireless In-Ear NC Headphones",
@@ -244,12 +266,7 @@ const productsData = [
   },
   {
     id: 12,
-    images: [
-      "/images/products/boat228-1.png",
-      "/images/products/boat228-2.png",
-      "/images/products/boat228-3.png",
-      "/images/products/boat228-4.png",
-    ],
+    images: [fourtySix, fourtySeven, fourtyEight, fourtyNine],
     brand: "boAt",
     title: "boAt BassHeads 228",
     info: "In-Ear Wired Earphones",
@@ -266,12 +283,7 @@ const productsData = [
   {
     id: 13,
     tag: "featured-product",
-    images: [
-      "/images/products/jbl-endu-1.png",
-      "/images/products/jbl-endu-2.png",
-      "/images/products/jbl-endu-3.png",
-      "/images/products/jbl-endu-4.png",
-    ],
+    images: [fifty, fiftyone, fiftytwo, fiftyThree],
     brand: "JBL",
     title: "JBL Endurance Run Sports",
     info: "In-Ear Wired Earphones",
@@ -288,12 +300,7 @@ const productsData = [
   {
     id: 14,
     tag: "featured-product",
-    images: [
-      "/images/products/boat203-1.png",
-      "/images/products/boat203-2.png",
-      "/images/products/boat203-3.png",
-      "/images/products/boat203-4.png",
-    ],
+    images: [fiftyFour, fiftyFive, fiftySix, fiftySeven],
     brand: "boAt",
     title: "boAt Airdopes 203",
     info: "In-Ear Truly Wireless Earbuds",
@@ -309,12 +316,7 @@ const productsData = [
   },
   {
     id: 15,
-    images: [
-      "/images/products/sonych710n-1.png",
-      "/images/products/sonych710n-2.png",
-      "/images/products/sonych710n-3.png",
-      "/images/products/sonych710n-4.png",
-    ],
+    images: [fiftyEight, fiftyNine, sixty, sixtyOne],
     brand: "Sony",
     title: "Sony WH-CH710N",
     info: "Wireless Over-Ear NC Headphones",
@@ -330,12 +332,7 @@ const productsData = [
   },
   {
     id: 16,
-    images: [
-      "/images/products/jbl500bt-1.png",
-      "/images/products/jbl500bt-2.png",
-      "/images/products/jbl500bt-3.png",
-      "/images/products/jbl500bt-4.png",
-    ],
+    images: [sixtyTwo, sixtyThree, sixtyFour, sixtyFive],
     brand: "JBL",
     title: "JBL Tune 500BT",
     info: "On-Ear Wireless Headphones",
@@ -351,12 +348,7 @@ const productsData = [
   },
   {
     id: 17,
-    images: [
-      "/images/products/boat381-1.png",
-      "/images/products/boat381-2.png",
-      "/images/products/boat381-3.png",
-      "/images/products/boat381-4.png",
-    ],
+    images: [sixtySix, sixtySeven, sixtyEight, sixtyNine],
     brand: "boAt",
     title: "boAt Airdopes 381",
     info: "In-Ear Wireless Earbuds",
@@ -372,12 +364,7 @@ const productsData = [
   },
   {
     id: 18,
-    images: [
-      "/images/products/sony-ex14ap-1.png",
-      "/images/products/sony-ex14ap-2.png",
-      "/images/products/sony-ex14ap-3.png",
-      "/images/products/sony-ex14ap-4.png",
-    ],
+    images: [seventy, seventyOne, seventyTwo, seventyThree],
     brand: "Sony",
     title: "Sony MDR-EX14AP",
     info: "In-Ear Wired Earphones",
@@ -393,12 +380,7 @@ const productsData = [
   },
   {
     id: 19,
-    images: [
-      "/images/products/sonyxb400-1.png",
-      "/images/products/sonyxb400-2.png",
-      "/images/products/sonyxb400-3.png",
-      "/images/products/sonyxb400-4.png",
-    ],
+    images: [seventyFour, sevetyFive, seventySix, seventySeven],
     brand: "Sony",
     title: "Sony WI-XB400",
     info: "Wireless Extra Bass In-Ear Neckbands",
